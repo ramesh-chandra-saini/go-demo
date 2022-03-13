e1a31b9f0d34d8f1c731a6ca40ba4281aa2ae3ba
Ramesh Chandra <ramesh.chandra@zomato.com>
2022-03-13 15:55:04 +0530

    [Learning] - Adding first go programs

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ramesh-chandra-saini
+
+go 1.16
+
+replace github.com/ramesh-chandra-saini/greetings => ../greetings
---
+++ hello.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/ramesh-chandra-saini/greetings"
+)
+
+func main() {
+	fmt.Println(greetings.Hello("Ramesh"))
+}
